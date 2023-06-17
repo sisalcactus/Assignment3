92b0782303e7aa64ed38d8c16cc37f5b1a695843
sisalcactus <hsiangchang2000@gmail.com>
2023-06-17 19:50:51 -0400

    Finalized the basics of the game. All code functional.

--- Assignment 3.R
+++ Assignment 3.R
@@ -4,33 +4,18 @@
 dictionary <- read.table("dictionary.txt", stringsAsFactors = FALSE)
 keyword <- sample(dictionary$V1, 1)
 
+
+dictionary <- read.table("dictionary.txt", stringsAsFactors = FALSE)
+keyword <- sample(dictionary$V1, 1)
+
 print(paste("Welcome to Hangman! This word has", nchar(keyword), "letters. Type 1 letter to begin your guess."))
 
-# first guess
-
-repeat{
-  guess <- readline(prompt = "Please enter a letter: ")
-  if ((grepl("[A-Za-z]", guess)) &
-      (nchar(guess) == 1)){
-    break
-  }
-}
-
-
-# check if the first guess is a letter in the keyword
-
-guess <- as.character(guess)
-
-guess_nocaps <- tolower(guess)
-
-keyword <- as.character(keyword)
-
-letters <- unlist(strsplit(keyword, ""))
-  
 i <- 5
 
-if(guess_nocaps %in% letters){
-    print(paste(guess_nocaps, "is in the secret word. Great work.")) # can specify the exact location using which()
+past_guesses <- c()
+
+# first guess
+while(i>0){
   
   repeat{
     guess <- readline(prompt = "Please enter a letter: ")
@@ -40,23 +25,33 @@ if(guess_nocaps %in% letters){
     }
   }
   
-} else {
-  print(paste("Not that one. Please try another. You have", i, "tries."))
-  i <- i - 1
+  # check if the first guess is a letter in the keyword
   
-  repeat{
-    guess <- readline(prompt = "Please enter a letter: ")
-    if ((grepl("[A-Za-z]", guess)) &
-        (nchar(guess) == 1)){
-      break
-    }
+  guess <- as.character(guess)
+  
+  guess_nocaps <- tolower(guess)
+  
+  keyword <- as.character(keyword)
+  
+  letters <- unlist(strsplit(keyword, ""))
+  
+  if(guess_nocaps %in% letters){
+    print(paste(guess_nocaps, "is in spot", which(guess_nocaps == letters), "of the secret word. Great work."))
+    
+  } else {
+    i <- i-1
+    print(paste(guess_nocaps, "is not in the secret word. You have", i, "tries left."))
   }
   
+  past_guesses <- append(past_guesses, guess_nocaps)
+  
+  if(all(letters %in% past_guesses)){
+    print(paste("You won!", keyword, "is indeed the secret word!"))
+    break
+  }
+  
+  if(i == 0){
+    print("You lost.")
+  }
 }
-
-
-# repeat{
-# tries <- c(1, 2, 3, 4, 5)
-# 
-
-
+print("Play again.")
